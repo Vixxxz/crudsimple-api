887f782c5320fd12f392eb86ad3248cf113524e5
Vixxxz <ryan.takata@outlook.com>
2024-09-20 13:42:38 -0300

    Configure Project

---
+++ src/test/java/com/crud/crudsimple/CrudsimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.crudsimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudsimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
